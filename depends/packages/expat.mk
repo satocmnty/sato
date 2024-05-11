b1574ecaede3871f3597fa1a8ae2b15a968f893b
Th1nk4h34d <159618756+Th1nk4h34d@users.noreply.github.com>
2024-05-11 16:40:00 +0200

    fix depends build

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,11 +1,11 @@
 package=expat
-$(package)_version=2.4.1
+$(package)_version=2.6.2
 $(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/$($(package)_version)
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=2f9b6a580b94577b150a7d5617ad4643a4301a6616ff459307df3e225bcfbf40
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=ee14b4c5d8908b1bec37ad937607eab183d4d9806a08adee472c3c3121d27364
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+$(package)_config_opts=--disable-static --without-docbook
 endef
 
 define $(package)_config_cmds
